a6de1d53c7a5715d24e24314c3eae8642842f865
dakc2 <dakc2@DESKTOP-MKKEPS5>
2020-06-24 17:10:41 +0530

    this my first wtn project.

---
+++ while/src/test/Test.java
@@ -0,0 +1,15 @@
+package test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=5;
+		while(i==5) {
+			System.out.println(+i);
+			i=i+1;
+		}
+
+	}
+
+}
